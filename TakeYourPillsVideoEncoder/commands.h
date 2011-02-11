819a70dde07ecef618e7776dc4ccf8408d966845
raphael.guillory@gmail.com <raphael.guillory@gmail.com@04fd080f-97ed-5b56-5b6c-a1039f277f53>
2011-02-11 21:46:08 +0000

    Initial version

---
+++ TakeYourPillsVideoEncoder/commands.h
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : Take Your Pills Video Encoder
+// Author      : Raphaël GUILLORY
+// Version     : alpha
+// Copyright   : Please contact raphael.guillory@gmail.com
+// Description : Video encoder for Take Your Pills
+//============================================================================
+
+
+#ifndef _COMMANDS_H
+#define	_COMMANDS_H
+
+
+#include <string>
+#include <vector>
+
+
+class command
+{
+public:
+	std::string name;
+	std::string id;
+	int pass;
+	std::string commandline;
+};
+
+
+class commands
+{
+public:
+	std::vector<command> commandsSd;
+	std::vector<command> commandsHd;
+	std::vector<command> commandsPv;
+};
+
+#endif	/* _COMMANDS_H */
---
+++ TakeYourPillsVideoEncoder/main.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Take Your Pills Video Encoder
+// Author      : Raphaël GUILLORY
+// Version     : alpha
+// Copyright   : Please contact raphael.guillory@gmail.com
+// Description : Video encoder for Take Your Pills
+//============================================================================
+
+
+#include <QApplication>
+#include "form.h"
+
+
+int main(int argc,char *argv[])
+{
+	// Launch widget
+	QApplication mainApplication(argc,argv);
+	form mainForm;
+	mainForm.show();
+	mainForm.setWindowIcon(QIcon("logo.png"));
+	return mainApplication.exec();
+}
